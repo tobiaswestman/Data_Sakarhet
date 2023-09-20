889d999cf84e5c9bbf5644d21d37ac921f0efc76
Tobias Westman <ctvwestman@gmail.com>
2023-09-20 23:01:10 +0200

    add repo

---
+++ Uppgift1/Uppgift1Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Uppgift1Client
+{
+    // Vårt objekt
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class Client
+    {
+        public void Connect()
+        {
+            // Skapa en anslutning till servern på "localhost" och port 4000.
+            TcpClient client = new TcpClient("localhost", 4000);
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            StreamWriter writer = new StreamWriter(stream, Encoding.UTF8);
+
+            // Skapa en person och serialisera den till en JSON-sträng.
+            Person person = new Person { Name = "Tobias", Age = 25, Address = "Stockholm, Sweden" };
+            string request = JsonConvert.SerializeObject(person);
+
+            // Skicka JSON-strängen till servern.
+            writer.WriteLine(request);
+            writer.Flush();
+
+            // Läs svaret från servern.
+            string response = reader.ReadLine();
+            Console.WriteLine($"Received: {response}");
+
+            client.Close();
+        }
+
+        // Starta klienten
+        public static void Main()
+        {
+            Client client = new Client();
+            client.Connect();
+        }
+    }
+}
---
+++ Uppgift1/Uppgift1Server/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Uppgift1Server
+{
+    // Vårt objekt
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class Server
+    {
+        public void Start()
+        {
+            // Starta en TCP-listener på port 4000 för alla IP-adresser.
+            TcpListener listener = new TcpListener(IPAddress.Any, 4000);
+            listener.Start();
+            Console.WriteLine("Server started...");
+
+            // En loop som acceptera klientanslutningar.
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Client connected...");
+                HandleClient(client);
+            }
+        }
+
+        // Hantera varje klientanslutning individuellt.
+        private void HandleClient(TcpClient client)
+        {
+            // Skapa strömmar för att läsa och skriva till klienten.
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            StreamWriter writer = new StreamWriter(stream, Encoding.UTF8);
+
+            // Läs en rad data från klienten.
+            string request = reader.ReadLine();
+            Console.WriteLine($"Received: {request}");
+
+            // Bearbeta förfrågan och skapa ett svar.
+            string response = ProcessRequest(request);
+
+            // Skicka svaret tillbaka till klienten.
+            writer.WriteLine(response);
+            writer.Flush();
+            client.Close();
+        }
+
+        // deserialisera JSON-strängen till ett personobjekt.
+        private string ProcessRequest(string request)
+        {
+            var person = JsonConvert.DeserializeObject<Person>(request);
+            return $"Hello, {person.Name}! You are {person.Age} years old and live at {person.Address}.";
+        }
+
+        //Starta servern
+        public static void Main()
+        {
+            Server server = new Server();
+            server.Start();
+        }
+    }
+}
+
