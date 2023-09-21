7fa5f07cba963cbae81592eb8a3b748e6f57335b
Tobias Westman <ctvwestman@gmail.com>
2023-09-21 19:22:03 +0200

    Add UdpClient and UdpServer

--- Uppgift2/UdpClient/Program.cs
+++ Uppgift2/UdpClient/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace UdpClientApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Create UdpClient and connect to set IP and port
+			int port = 4000;
+			UdpClient udpClient = new UdpClient();
+			udpClient.Connect("127.0.0.1", port);
+
+			// Let user input name and age, parse age to int
+			Console.Write("Enter name: ");
+			string name = Console.ReadLine();
+
+			Console.Write("Enter age: ");
+			int age;
+			Int32.TryParse(Console.ReadLine(), out age);
+
+			// Create person object with user input
+			var dataToSend = new Person() { Name = name, Age = age };
+
+			// Serialize object to JSON-string
+			string jsonString = JsonConvert.SerializeObject(dataToSend);
+
+			//Convert JSON-string to byte-data
+			byte[] bytesToSend = Encoding.UTF8.GetBytes(jsonString);
+
+			// Send byte-data and notify user
+			udpClient.Send(bytesToSend, bytesToSend.Length);
+			Console.WriteLine("Data sent to server!");
+		}
+	}
+
+	public class Person
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+	}
+}
--- Uppgift2/UdpServer/Program.cs
+++ Uppgift2/UdpServer/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace UdpServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Create UdpClient and listen to port 4000
+			int port = 4000;
+			UdpClient udpServer = new UdpClient(port);
+			Console.WriteLine($"Server started, listening on port {port}...");
+
+			while (true)
+			{
+				// Define an endpoint and receive data from client
+				IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, port);
+				byte[] bytesReceived = udpServer.Receive(ref clientEndPoint);
+
+				// Convert byte-data to JSON-string
+				string jsonString = Encoding.UTF8.GetString(bytesReceived);
+
+				// Deserialize JSON to object and write out result
+				var data = JsonConvert.DeserializeObject<Person>(jsonString);
+				Console.WriteLine($"Received message from {clientEndPoint} - Name: {data.Name}, Age: {data.Age}");
+			}
+		}
+	}
+
+	public class Person
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+	}
+}
